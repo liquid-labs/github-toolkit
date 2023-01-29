e44b6275a764aada2c7bd86f50319af2664a970e
Zane Rockenbaugh <zane@liquid-labs.com>
2023-01-29 14:29:36 -0600

    added documentation; checks whether ssh-agent is running

--- src/access-lib.mjs
+++ src/access-lib.mjs
@@ -4,6 +4,19 @@ import createError from 'http-errors'
 import yaml from 'js-yaml'
 import shell from 'shelljs'
 
+const SSH_AGENT_NOT_RUNNING = (pathToPrivKey) =>
+  `It looks like the 'ssh-agent' isn't running. Add the following to '.bashrc' or equivalent:
+
+SSHAGENT=/usr/bin/ssh-agent
+SSHAGENTARGS="-s"
+if [ -z "$SSH_AUTH_SOCK" -a -x "$SSHAGENT" ]; then
+    eval `$SSHAGENT $SSHAGENTARGS`
+    trap "kill $SSH_AGENT_PID" 0
+fi
+
+Then add your GitHub key with:
+
+ssh-add ${pathToPrivKey}`
 const SSH_ACCESS_FAILURE_MSG = (pathToPrivKey) => 
   `Test for SSH access to GitHub failed. Try to add your GitHub key like:\n\nssh-add ${pathToPrivKey}`
 
@@ -14,6 +27,12 @@ const API_NO_TOKEN = `API credentials JSON file at '${API_CREDS_DEFAULT_PATH}' d
 const API_BAD_CHECK = 'Failed to execute API authorization check.'
 const API_TOKEN_INVALID = 'The access token appears invalid.'
 
+/**
+ * Checks GitHub API access.
+ * 
+ * ### Parameters
+ * - `filePath`: path to API token file. The default '~/.config/hub' is used otherwise.`
+ */
 const checkGitHubAPIAccess = async({ filePath = API_CREDS_DEFAULT_PATH }) => {
   let creds
   try {
@@ -46,15 +65,29 @@ const checkGitHubAPIAccess = async({ filePath = API_CREDS_DEFAULT_PATH }) => {
   // else, we're good
 }
 
+/**
+ * Checks SSH-based access to GitHub. This method relies on ssh-agent. In the case of failure, will attempt to 
+ * diagnose the specific issue.
+ * 
+ * ### Parameters
+ * - `privKeyPath`: the path to the GitHub privaet key. Setting the parameter does not effect the test, but it is used 
+ *   in reporting problems.`
+ */
 const checkGitHubSSHAccess = ({ privKeyPath }) => {
   // the expected resut is idiomaticaly 1 because GitHub does not allow terminal access. But if the connection cannot be made, then the exit
   // code is different.
-  const command = privKeyPath
-    ? `ssh -o "IdentitiesOnly=yes" -i ${privKeyPath} -qT git@github.com 2> /dev/null`
-    : 'ssh -qT git@github.com 2> /dev/null'
+  const command = 'ssh -qT git@github.com 2> /dev/null'
   const result = shell.exec(command)
   if (result.code !== 1) {
-    throw createError.Unauthorized(SSH_ACCESS_FAILURE_MSG(privKeyPath || '/path/to/private-key'))
+    const result = shell.exec('pgrep ssh-agent')
+    let msg
+    if (result.code !== 0) {
+      msg = SSH_AGENT_NOT_RUNNING(privKeyPath || '/path/to/private-key')
+    }
+    else {
+      msg = SSH_ACCESS_FAILURE_MSG(privKeyPath || '/path/to/private-key')
+    }
+    throw createError.Unauthorized(msg)
   }
   return true
 }
