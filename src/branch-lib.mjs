bb27446e1c99981a72950b6a2e3dcd64be971b5d
Zane Rockenbaugh <zane@liquid-labs.com>
2023-02-10 11:17:24 -0600

    removed pure git functions to git-toolkit; import what's needed

--- src/branch-lib.mjs
+++ src/branch-lib.mjs
@@ -1,53 +1,10 @@
 import createError from 'http-errors'
 import shell from 'shelljs'
 
+import { determineOriginAndMain } from '@liquid-labs/git-toolkit'
+
 const ORIGIN = 'origin'
 const MAIN = 'main'
-
-const KNOWN_ORIGINS = ['origin', 'upstream']
-const KNOWN_MAINS = ['main', 'master']
-
-// TODO: move this to 'git-toolkit'
-const determineCurrentBranch = ({ projectPath, reporter }) => {
-  reporter?.push('Fetching current branch name...')
-  const branchResult = shell.exec(`cd '${projectPath}' && git branch | grep '*' | cut -d' ' -f2`)
-  if (branchResult.code !== 0) { throw createError.InternalServerError(`Could not determnie current branch for git repo at '${projectPath}'.`) }
-
-  return branchResult.stdout.trim()
-}
-
-const determineOriginAndMain = ({ projectPath, reporter }) => {
-  reporter?.push('Fetching latest origin data...')
-  const fetchResult = shell.exec(`cd ${projectPath} && git fetch -p`)
-
-  if (fetchResult.code !== 0) { throw createError(`'git fetch -p' failed at '${projectPath}': ${fetchResult.stderr}.`) }
-
-  reporter?.push('Checking remote branches...')
-  const remoteBranchQuery = shell.exec(`cd ${projectPath} && git branch -r`)
-  if (remoteBranchQuery.code !== 0) throw createError(`Could not list remote branches: ${remoteBranchQuery.stderr}`)
-  const remoteBranches = remoteBranchQuery.split('\n').map((r) => r.trim().split('/'))
-
-  let origin, main
-  for (const [remote, branch] of remoteBranches) {
-    if (origin === undefined && KNOWN_ORIGINS.includes(remote)) {
-      origin = remote
-    }
-    else if (origin !== undefined && origin !== remote && KNOWN_ORIGINS.includes(remote)) {
-      throw createError(`Found multiple possible origin remotes: ${origin} + ${remote}`)
-    }
-
-    if (main === undefined && KNOWN_MAINS.includes(branch) && KNOWN_ORIGINS.includes(remote)) {
-      main = branch
-    }
-    else if (main !== undefined && main !== branch && KNOWN_MAINS.includes(branch) && KNOWN_ORIGINS.includes(remote)) {
-      throw createError(`Found multiple possible origin branches: ${main} + ${branch}`)
-    }
-  }
-
-  reporter?.push(`Determined origin and main branch: ${origin}/${main}.`)
-
-  return [origin, main]
-}
 
 const regularizeRemote = ({ projectPath, reporter }) => {
   let [originRemote] = determineOriginAndMain({ projectPath })
@@ -104,8 +61,6 @@ const regularizeMainBranch = ({ projectFQN, projectPath, reporter }) => {
 }
 
 export {
-  determineCurrentBranch,
-  determineOriginAndMain,
   regularizeRemote,
   regularizeMainBranch
 }
