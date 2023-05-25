27f9a9ea2f4a0e9fa0d3336881be54c528c21307
Zane Rockenbaugh <zane@liquid-labs.com>
2023-05-25 09:37:15 -0500

    export 'determineCurrentRelease'

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 export * from './access-lib'
 export * from './branch-lib'
+export * from './determine-current-release'
 export * from './issues-lib'
 export * from './labels-lib'
 export * from './milestones-lib'
