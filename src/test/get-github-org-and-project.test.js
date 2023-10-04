137355ac979c7c14bba87b2c404bda0702e82c42
Zane Rockenbaugh <zane@liquid-labs.com>
2023-10-04 12:36:56 -0500

    lint formatting

--- src/test/get-github-org-and-project.test.js
+++ src/test/get-github-org-and-project.test.js
@@ -5,22 +5,22 @@ import { getGitHubOrgAndProject } from '../get-github-org-and-project'
 // requires user to have API access
 describe('getGitHubOrgAndProject', () => {
   test('works with string repositories', () => {
-    const packageJSON = { repository: "git+ssh://git@github.com/acme/foo-bar.git" }
-    expect(getGitHubOrgAndProject({ packageJSON })).toEqual({ org: 'acme', project: 'foo-bar' })
+    const packageJSON = { repository : 'git+ssh://git@github.com/acme/foo-bar.git' }
+    expect(getGitHubOrgAndProject({ packageJSON })).toEqual({ org : 'acme', project : 'foo-bar' })
   })
 
   test('works with object repositories', () => {
-    const packageJSON = { repository: { url: "git+ssh://git@github.com/acme/foo-bar.git" } }
-    expect(getGitHubOrgAndProject({ packageJSON })).toEqual({ org: 'acme', project: 'foo-bar' })
+    const packageJSON = { repository : { url : 'git+ssh://git@github.com/acme/foo-bar.git' } }
+    expect(getGitHubOrgAndProject({ packageJSON })).toEqual({ org : 'acme', project : 'foo-bar' })
   })
 
   test('returns undefined values when no match', () => {
-    const packageJSON = { repository: "git+ssh://git@anotherplace.com/acme/foo-bar.git" }
-    expect(getGitHubOrgAndProject({ packageJSON })).toEqual({ org: undefined, project: undefined })
+    const packageJSON = { repository : 'git+ssh://git@anotherplace.com/acme/foo-bar.git' }
+    expect(getGitHubOrgAndProject({ packageJSON })).toEqual({ org : undefined, project : undefined })
   })
 
   test('throws error when no match and required', () => {
-    const packageJSON = { repository: "git+ssh://git@anotherplace.com/acme/foo-bar.git" }
-    expect(() => getGitHubOrgAndProject({ packageJSON, require: true })).toThrow()
+    const packageJSON = { repository : 'git+ssh://git@anotherplace.com/acme/foo-bar.git' }
+    expect(() => getGitHubOrgAndProject({ packageJSON, require : true })).toThrow()
   })
 })
