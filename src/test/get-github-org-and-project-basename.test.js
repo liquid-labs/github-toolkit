18316314929d09ae7afa6569f5d4d21a60b3ce6b
Zane Rockenbaugh <zane@liquid-labs.com>
2024-01-18 16:00:09 -0600

    add unit test to prove out recent fix

--- src/test/get-github-org-and-project-basename.test.js
+++ src/test/get-github-org-and-project-basename.test.js
@@ -4,19 +4,14 @@ import { getGitHubOrgAndProjectBasename } from '../get-github-org-and-project-ba
 
 // requires user to have API access
 describe('getGitHubOrgAndProjectBasename', () => {
-  test('works with string repositories', () => {
-    const packageJSON = { repository : 'git+ssh://git@github.com/acme/foo-bar.git' }
-    expect(getGitHubOrgAndProjectBasename({ packageJSON })).toEqual({ org : 'acme', projectBasename : 'foo-bar' })
-  })
-
-  test('works with object repositories', () => {
-    const packageJSON = { repository : { url : 'git+ssh://git@github.com/acme/foo-bar.git' } }
-    expect(getGitHubOrgAndProjectBasename({ packageJSON })).toEqual({ org : 'acme', projectBasename : 'foo-bar' })
-  })
-
-  test('returns undefined values when no match', () => {
-    const packageJSON = { repository : 'git+ssh://git@anotherplace.com/acme/foo-bar.git' }
-    expect(getGitHubOrgAndProjectBasename({ packageJSON })).toEqual({ org : undefined, projectBasename : undefined })
+  test.each([
+    ['works with string repositories', 'git+ssh://git@github.com/acme/foo-bar.git', 'acme', 'foo-bar'],
+    ['works with object repositories', { url: 'git+ssh://git@github.com/acme/foo-bar.git' }, 'acme', 'foo-bar'],
+    ["deals with '.' in the package name", 'git+ssh://git@github.com/acme/acme.com.git', 'acme', 'acme.com'],
+    ['returns undefined values when no match', 'git+ssh://git@anotherplace.com/acme/foo-bar.git', undefined, undefined]
+  ])('%s', (description, repository, org, projectBasename) => {
+    const packageJSON = { repository  }
+    expect(getGitHubOrgAndProjectBasename({ packageJSON })).toEqual({ org, projectBasename })
   })
 
   test('throws error when no match and required', () => {
